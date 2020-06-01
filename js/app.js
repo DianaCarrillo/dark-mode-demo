0d0a68cce6c0ee8176c6bb1fb2ab92457b83b25b
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2020-06-01 16:22:55 -0500

    Challenge completed

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const checkbox = document.querySelector('#checkbox')
+if(window.matchMedia('(prefers-color-scheme: dark').matches){
+    console.log(true)
+    checkbox.setAttribute('checked', true)
+}
+checkbox.addEventListener('change', function(e){
+    // console.log(this.checked)
+        document.body.classList.toggle('is-dark-mode')
+        if(this.checked){
+            document.body.classList.remove('is-light-mode')
+            document.body.classList.add('is-dark-mode')
+        }else{
+            document.body.classList.remove('is-dark-mode')
+            document.body.classList.add('is-light-mode')
+        }
+})
